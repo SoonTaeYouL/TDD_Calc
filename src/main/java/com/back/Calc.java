59e4d3f26708d3f3536356f59f47b4542ff408ea
SoonTaeYouL <smile971021@naver.com>
2025-06-02 01:38:58 +0900

    2 + 2 == 4, g

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,9 +2,11 @@ package com.back;
 
 public class Calc {
     public static int run(String exp){
-        if (exp.equals("2 + 1")) {
-            return 3;
-        }
-        return 2;
+        String[] expression = exp.split(" \\+ ");
+
+        int num1 = Integer.parseInt(expression[0]);
+        int num2 = Integer.parseInt(expression[1]);
+
+        return num1 + num2;
     }
 }
